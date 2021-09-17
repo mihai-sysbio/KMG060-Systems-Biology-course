2193cfc3eceb3c1cf62c394dbfae280c65f5d2d1
KfabianChalm <90684436+KfabianChalm@users.noreply.github.com>
2021-09-17 11:44:05 +0200

    Update FabianExercise.m

--- FabianExercise.m
+++ FabianExercise.m
@@ -54,7 +54,7 @@ legend(condStr)
 hold off
 
 %% 3. Differentia gene expression
-normCountsStress  = normCounts(:,LowpH);
+normCountsStress  = normCounts(:,Htemp);
 normCountsref     = normCounts(:,ref);
 tLocal = nbintest(normCountsref,normCountsStress,'VarianceLink','LocalRegression');
 padj = mafdr(tLocal.pValue,'BHFDR',true);
@@ -68,7 +68,7 @@ geneTable.Properties.VariableNames = {'Mean_Ref','Mean_Stress','Log2_FC','pVal',
 x=geneTable.Log2_FC; y=geneTable.adjPVal;
 FClim=abs(x)>=2; adjlim=y<=0.01;y=-log10(geneTable.adjPVal);
 hold on
-scatter(x,y,30,'fill','black')
+%scatter(x,y,30,'fill','black')
 
 % For loop to only include values on positions in the matrix where both
 % conditions abs(Log2FC)=>2 and adj_pVal<=0.01 are true
@@ -82,19 +82,57 @@ for i=1:length(x)
     end
 end
 
-scatter(x,y,30,'fill','red')
-line([2 2],[0 60]); line([-2 -2],[0 60]); line([-6 6],-log10([0.01 0.01]));
+%scatter(x,y,30,'fill','red')
+%line([2 2],[0 60]); line([-2 -2],[0 60]); line([-6 6],-log10([0.01 0.01]));
 title('Volcano plot')
-xlabel('Log2 FC')
+xlabel('Log2 FoldChange')
 ylabel('-log10(adjPValue)')
 hold off
 
-%% 4. 
+%% 4. First analysis of DE genes
+Table4 = geneTable; %In order to leave geneTable untouched
 Desc=readtable('GeneDescriptions.csv'); %Get data as table
-Desc=table(Desc{:,1}, Desc{:,3}); %Remove abundant second column, we only want gene names (outerjoin) and description columns
-q=geneTable.Properties.RowNames; geneTable{:,6}=q; %Adds row names to 6th column for outerjoin
-geneTable = outerjoin(Desc,geneTable,'LeftKeys','Var1','RightKeys','Var6'); %Adds Gene Descriptions to Genetable
-geneTable = sortrows(geneTable,'adjPVal','ascend');
-geneTable.Var6=[]; %Removes abundand column (which was only used for the purpose of outerjoin)
-geneTable.Properties.VariableNames([1 2])={'Gene','Description'}; %Adds headers to Gene names and Description columns
-disp(geneTable(1:10,:))
+Desc=table(Desc{:,1}, Desc{:,3}); %Remove abundant second column, we only want gene names (for outerjoin) and description columns
+q=Table4.Properties.RowNames; Table4{:,6}=q; %Adds row names to 6th column for outerjoin
+Table4 = outerjoin(Desc,Table4,'LeftKeys','Var1','RightKeys','Var6'); %Adds Gene Descriptions to Genetable
+Table4 = sortrows(Table4,'adjPVal','ascend');
+Table4.Var6 = []; %Removes abundand column (which was only used for the purpose of outerjoin)
+Table4.Properties.VariableNames([1 2]) = {'Gene','Description'}; %Adds headers to Gene names and Description columns
+disp(Table4(1:10,:))
+
+%% 5. Find Associated GO Terms
+%geneTable = geneTable.Log2_FC > 0;
+GOGenes = geneTable;
+GOGenes = GOGenes(GOGenes.Log2_FC < 100 ,:);
+GOGenes = sortrows(GOGenes,'Log2_FC','descend');
+
+GoTerms_table = readtable('../data/GoTermsMapping.txt','delimiter','\t');
+GoTermsIDs    = unique(GoTerms_table.GoTerm);
+
+GoTermsGeneMap = containers.Map();
+for i = 1:height(GoTerms_table)
+    key = GoTerms_table{i,'GeneName'}{1};
+    if isKey(GoTermsGeneMap,key)
+        GoTermsGeneMap(key) = [GoTermsGeneMap(key),GoTerms_table{i,'GoTerm'}{1}];
+    else
+        GoTermsGeneMap(key) = {GoTerms_table{i,'GoTerm'}{1}};
+    end
+end
+fprintf('Number of annotated genes related to functional process is %d.\n',GoTermsGeneMap.Count)
+fprintf('Number of unique GO terms associated to annotated genes is %d.\n',numel(unique(GoTerms_table.GoTerm)))
+fprintf('Number of gene-GO term associations is %d.\n',numel(GoTerms_table))
+
+selectedGene      = GOGenes.Properties.RowNames{1};
+associatedGoTerms = GoTermsGeneMap(selectedGene);
+disp(['Associated GO Terms for gene ',selectedGene])
+disp(associatedGoTerms)
+
+GO = geneont('File','../data/GoTerms.obo');
+
+for i=1:length(associatedGoTerms)
+GoTermID         = associatedGoTerms{i}(4:end);
+GoTermName       = GO(str2double(GoTermID)).Terms.Name;
+GoTermDefinition = GO(str2double(GoTermID)).Terms.Definition;
+
+disp(['GO Term ',(GoTermID),' - ',GoTermName,' : ',GoTermDefinition])
+end
